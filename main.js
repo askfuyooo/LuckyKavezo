67d6a11c75d1b7a22330a1067c12ae8c2f3c7ac9
Tóth Ádám <adamtoth1215@gmail.com>
2022-09-26 13:42:58 +0200

    Kávés oldal !NAGYON ALFA!

---
+++ main.js
@@ -0,0 +1,13 @@
+function elso(kep) {
+    let asd = document.getElementById("txt")
+    switch(kep) {
+        case logo: {
+            asd.innerHTML = "alap"
+            break
+        }
+        default: {
+            asd.innerHTML = "alap"
+            break
+        }
+    }
+}
