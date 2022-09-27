acd1363125898eca802b25e8c485e2e7478802e7
Tóth Ádám <adamtoth1215@gmail.com>
2022-09-27 14:13:44 +0200

    Kávék, JS

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-function kepLeiras(kep) {
-    let leiras = document.getElementById("txt")
-    switch(kep) {
-        case logo: {
-            leiras.innerHTML = "alap"
-            break
-        }
-        default: {
-            leiras.innerHTML = ""
-            break
-        }
-    }
-}
-
-let kepek = 3
+let kepek = 18
 function picHover(kep) {
     //document.getElementById("desc").style.display = "flex"
     for (i = 1; i <= kepek; i++) {
         if (i == kep) {
             document.getElementById(i).style.filter = "grayscale(0)"
             document.getElementById(i).style.transform = "scale(1.1)"
-            document.getElementById(i).style.filter = "blur(5px)"
+            document.getElementById(i).style.filter = "blur(1px)"
             document.getElementById("txt" + i).style.display = "flex"
             document.getElementById(i).style.backgroundColor = "white"
         } else {
-            document.getElementById(i).style.filter = "grayscale(10)"
             document.getElementById(i).style.transform = "scale(1)"
+            document.getElementById(i).style.filter = "grayscale(10) blur(5px)"
         }
     }
 }
