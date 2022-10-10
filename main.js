372b5e8c7d6ac637ec77369f14736d7de1e44abf
Tóth Ádám <adamtoth1215@gmail.com>
2022-10-10 13:25:17 +0200

    Javítások

--- main.js
+++ main.js
@@ -214,5 +214,22 @@ function picClick(kep) {
             bigDesc.innerHTML = "<h1> Cím </h2> <p> Leírás </p> <p><i> Ár </i></p>"
             break
         }
+
+        //Torta szeletek
+        case 's4': {
+            bigPic.setAttribute("src", "images/Sütemények/Torta_szeletek/Nutella_szelet.png")
+            bigDesc.innerHTML = "<h1> Cím </h2> <p> Leírás </p> <p><i> Ár </i></p>"
+            break
+        }
+        case 's5': {
+            bigPic.setAttribute("src", "images/Sütemények/Torta_szeletek/Soskaramell_szelet.png")
+            bigDesc.innerHTML = "<h1> Cím </h2> <p> Leírás </p> <p><i> Ár </i></p>"
+            break
+        }
+        case 's6': {
+            bigPic.setAttribute("src", "images/Sütemények/Torta_szeletek/Voros_Barsony_szelet.png")
+            bigDesc.innerHTML = "<h1> Cím </h2> <p> Leírás </p> <p><i> Ár </i></p>"
+            break
+        }
     }
 }
